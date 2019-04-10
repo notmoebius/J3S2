cb285471d90a5ba4f3de00a478aaaa3bdb2b423b
notmoebius <notmoebius@gmail.com>
2019-04-10 12:04:48 +0200

    exo test de connexion

--- lib/test-tweet.rb
+++ lib/test-tweet.rb
@@ -1,18 +1,26 @@
  # ligne très importante qui appelle la gem.
-require 'twitter'
 require 'dotenv'
+require 'twitter'
 require 'pry'
 
 # n'oublie pas les lignes pour Dotenv ici…
 Dotenv.load('../.env')
 
 # quelques lignes qui appellent les clés d'API de ton fichier .env
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+def login_twitter
+    # quelques lignes qui appellent les clés d'API de ton fichier .env
+    client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    end
+    puts ENV["TWITTER_CONSUMER_KEY"]
+    puts ENV["TWITTER_CONSUMER_SECRET"]
+    puts ENV["TWITTER_ACCESS_TOKEN"]
+    puts ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    client.update('Mon premier tweet en Ruby !!!!')
 end
 
 # ligne qui permet de tweeter sur ton compte
-client.update('Mon premier tweet en Ruby !!!!')
+login_twitter
